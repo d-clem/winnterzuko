ebfeead5062ff5e84f25433b26cac78db7e83f29
cleeem <145859535+d-clem@users.noreply.github.com>
2023-11-26 12:14:48 +0100

    update pages

--- app/index.php
+++ app/index.php
@@ -2,6 +2,8 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Wzuko</title>
 
     <link rel="stylesheet" href="../style/all.css">
