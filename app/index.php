b6b8427e64b9e7b20b6532a6d56ac20701a5c841
cleeem <145859535+d-clem@users.noreply.github.com>
2023-11-26 11:52:13 +0100

    image qui fit mieux

--- app/index.php
+++ app/index.php
@@ -69,7 +69,13 @@
 
     </div>
     <div class="main-2">
-        <section class="gray-card">
+
+        <section class="gray-card imageSection">
+            <img src="../static/img/zuko_5.jpg" 
+                alt="image de winnterzuko">
+        </section>
+
+        <section class="green-card">
             <h2>
                 Présentation de Winnterzuko (Music Story)
             </h2>
@@ -78,13 +84,6 @@
             Le rappeur français Winnterzuko garde son identité secrète, n'évoquant de son enfance qu'un exil qui l'a amené à quitter son pays d'origine. L'un de ses premiers titres, « Off », attire l'attention des fondateurs du label électronique parisien Promesses qui signe rapidement cet artiste alimentant son rap d'influences eurodance et drum'n'bass. En 2021, il sort plusieurs EP : Gearless Mania (2021) et 2036 (2021). Sur ce dernier, inspiré par l'histoire d'une internaute, qui, en 2001, prétendait venir du futur, figure « Trotski » qui rassemble plus d'un million de vues sur YouTube. L'année suivante, il publie un nouvel EP, du nom de Von. Il faut toutefois attendre 2023 pour que le rappeur mette au point un premier disque enregistré en studio. Baptisé Winntermania, il rassemble 12 morceaux mobilisant de nombreux producteurs comme Koboi, Amne, Bademe, Abel31 ou Guapo du Soleil. Il est emmené par des titres rap colorés par des touches dance ou electro comme « Gearless » ou « Devil Jin », « Synchonicity » ou « Van Gogh ». L'artiste enchaine ensuite avec une tournée en France, en Belgique et en Suisse baptisée Tour2ouf.
             </p>
         </section>
-
-        <section class="green-card imageSection">
-            <img src="../static/img/zuko_4.jpg" 
-                alt="image de winnterzuko">
-        </section>
-
-
 </div>
 
 
